3e5a96ec8821b00dbadd775e75415fb6a55a8a7f
bayelaye313 <bayelaye0510@gmail.com>
2024-03-06 15:38:39 +0000

    first banekh

--- src/content.js
+++ src/content.js
@@ -1,18 +1,23 @@
-// Variable pour stocker le dernier texte récupéré
-let lastProcessedText = '';
+let searchedQuestions = [];
 
-// Fonction pour récupérer et traiter le dernier texte en gras se terminant par un point d'interrogation
+function detectNewMessage() {
+  const messageForm = document.querySelector("#main > div._3B19s > div > div._5kRIK > div.n5hs2j7m.oq31bsqd.gx1rr48f.qh5tioqs > div:nth-child(27) > div > div > div.UzMP7._1uv-a > div._1BOF7._2AOIt > div:nth-child(1) > div > div.copyable-text > div > span._11JPr.selectable-text.copyable-text > span > strong");
+  if (messageForm) {
+    messageForm.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        chrome.runtime.sendMessage({ action: "newMessage" });
+      }
+    });
+  }
+}
+
 function processLastBoldTextEndingWithQuestionMark() {
-  const lastBoldTextEndingWithQuestionMark = document.querySelector("div.message-in.focusable-list-item._1AOLJ._2UtSC._1jHIY strong._11JPr");
-
-  if (lastBoldTextEndingWithQuestionMark) {
-    const boldTextContent = lastBoldTextEndingWithQuestionMark.textContent.trim();
-
-    // Vérifier si le texte est différent du dernier traité
-    if (boldTextContent !== lastProcessedText && boldTextContent.endsWith("?")) {
-      lastProcessedText = boldTextContent;
-
-      // Envoie le texte en gras se terminant par un point d'interrogation au script d'arrière-plan
+  const lastBoldTextEndingWithQuestionMark = document.querySelectorAll("div.message-in.focusable-list-item._1AOLJ._2UtSC._1jHIY strong._11JPr");
+  const lastElement = lastBoldTextEndingWithQuestionMark[lastBoldTextEndingWithQuestionMark.length - 1];
+  if (lastElement) {
+    const boldTextContent = lastElement.textContent.trim();
+    if (!searchedQuestions.includes(boldTextContent) && boldTextContent.endsWith("?")) {
+      searchedQuestions.push(boldTextContent);
       chrome.runtime.sendMessage({
         action: "boldText",
         boldText: boldTextContent
@@ -21,10 +26,10 @@ function processLastBoldTextEndingWithQuestionMark() {
   }
 }
 
-// Observer pour détecter les changements sur la page WhatsApp Web
+detectNewMessage();
+
 const observer = new MutationObserver(() => {
   processLastBoldTextEndingWithQuestionMark();
 });
 
-// Démarre l'observation des changements sur la page
 observer.observe(document.body, { subtree: true, childList: true });
